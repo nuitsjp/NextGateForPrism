7c4efc1b431fa2223f0ffb3a73cc8c89f9e833c4
nuits.jp <nuits.jp@live.jp>
2017-02-23 23:43:56 +0900

    Apply DesignTimeViewModelLocator

---
+++ EmployeeManager.ViewModels/DesignTimeViewModelLocator.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManager.ViewModels
+{
+    public static class DesignTimeViewModelLocator
+    {
+        public static MainPageViewModel MainPageViewModel => default(MainPageViewModel);
+        public static SectionPageViewModel SectionPageViewModel => default(SectionPageViewModel);
+        public static SectionListPageViewModel SectionListPageViewModel => default(SectionListPageViewModel);
+    }
+}
--- EmployeeManager.ViewModels/LocalViewModelLocator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EmployeeManager.ViewModels
-{
-    public static class LocalViewModelLocator
-    {
-        public static MainPageViewModel MainPageViewModel => default(MainPageViewModel);
-    }
-}
