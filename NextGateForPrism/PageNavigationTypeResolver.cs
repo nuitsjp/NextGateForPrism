71a910f3b4d1da945c518931bb70897763c9d86f
nuits.jp <nuits.jp@live.jp>
2017-02-28 10:16:31 +0900

    Refactor PageNavigationTypeResolver

--- NextGateForPrism/PageNavigationTypeResolver.cs
+++ NextGateForPrism/PageNavigationTypeResolver.cs
@@ -6,26 +6,15 @@ namespace NextGateForPrism
 {
     public static class PageNavigationTypeResolver
     {
-        private static readonly Dictionary<Assembly, Assembly> AssembliesForResolvingViewModelType = new Dictionary<Assembly, Assembly>();
-        private static readonly Dictionary<Assembly, Assembly> AssembliesForResolvingViewType = new Dictionary<Assembly, Assembly>();
+        private static readonly Dictionary<Assembly, Assembly> ViewModelAssignedToViewAssemblies = new Dictionary<Assembly, Assembly>();
+        private static readonly Dictionary<Assembly, Assembly> ViewAssignedToViewModelAssemblies = new Dictionary<Assembly, Assembly>();
 
         public static void AssignAssemblies<TView, TViewModel>()
         {
             var viewAssembly = typeof(TView).GetTypeInfo().Assembly;
             var viewModelAssembly = typeof(TViewModel).GetTypeInfo().Assembly;
-            AssembliesForResolvingViewModelType[viewAssembly] = viewModelAssembly;
-            AssembliesForResolvingViewType[viewModelAssembly] = viewAssembly;
-        }
-
-        private static Assembly ResolveViewModelAssembly(Assembly viewAssembly)
-        {
-            Assembly result;
-            if (!AssembliesForResolvingViewModelType.TryGetValue(viewAssembly, out result))
-            {
-                result = viewAssembly;
-                AssembliesForResolvingViewModelType[viewAssembly] = result;
-            }
-            return result;
+            ViewModelAssignedToViewAssemblies[viewAssembly] = viewModelAssembly;
+            ViewAssignedToViewModelAssemblies[viewModelAssembly] = viewAssembly;
         }
 
         public static Type ResolveForViewModelType(Type viewType)
@@ -34,33 +23,33 @@ namespace NextGateForPrism
 
             var viewName = viewType.FullName.Replace(".Views.", ".ViewModels.");
             var suffix = viewName.EndsWith("View") ? "Model" : "ViewModel";
-            var assembly = ResolveViewModelAssembly(viewType.GetTypeInfo().Assembly);
+            var assembly = ResolveAssembly(ViewModelAssignedToViewAssemblies, viewType.GetTypeInfo().Assembly);
             return assembly.GetType($"{viewName}{suffix}");
-        }
-
-        private static Assembly ResolveViewAssembly(Assembly viewModelAssembly)
-        {
-            Assembly result;
-            if (!AssembliesForResolvingViewType.TryGetValue(viewModelAssembly, out result))
-            {
-                result = viewModelAssembly;
-                AssembliesForResolvingViewModelType[viewModelAssembly] = result;
-            }
-            return result;
         }
 
         public static Type ResolveForViewType<TViewModel>() where TViewModel : class
         {
             var viewModelName = typeof(TViewModel).FullName.Replace(".ViewModels.", ".Views.");
             var suffixLength = viewModelName.EndsWith("PageViewModel") ? "ViewModel".Length : "Model".Length;
-            var assembly = ResolveViewAssembly(typeof(TViewModel).GetTypeInfo().Assembly);
+            var assembly = ResolveAssembly(ViewAssignedToViewModelAssemblies, typeof(TViewModel).GetTypeInfo().Assembly);
             return assembly.GetType(viewModelName.Substring(0, viewModelName.Length - suffixLength));
         }
 
         public static void Clear()
         {
-            AssembliesForResolvingViewModelType.Clear();
-            AssembliesForResolvingViewType.Clear();
+            ViewModelAssignedToViewAssemblies.Clear();
+            ViewAssignedToViewModelAssemblies.Clear();
+        }
+
+        private static Assembly ResolveAssembly(Dictionary<Assembly, Assembly> assemblies, Assembly key)
+        {
+            Assembly result;
+            if (!assemblies.TryGetValue(key, out result))
+            {
+                result = key;
+                assemblies[key] = result;
+            }
+            return result;
         }
     }
 }
